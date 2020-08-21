68a28b32b7d34c200cef5dca9504c39c12db8db1
Shrugsy <joshfraser91@gmail.com>
2020-08-22 02:32:18 +1000

    add heading, update packages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import store from "./store";
 import PostContainer from "./components/Posts/PostContainer";
 import ErrorMessage from "./components/ErrorMessage";
 import ThemeWrapper from "./components/ThemeWrapper";
+import Typography from "@material-ui/core/Typography";
 
 const App = () => {
   return (
     <div data-testid="App" className="App">
       <Provider store={store}>
         <ThemeWrapper>
+          <Typography variant="h4" gutterBottom>Todos</Typography>
           <PostContainer />
           <ErrorMessage />
         </ThemeWrapper>
